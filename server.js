4a379e5cccf9b6e2e8831397bc96f6aee7aa7a4a
Arun5010 <30664775+Arun5010@users.noreply.github.com>
2017-08-09 21:34:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("Requesting for Article two server page");
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
